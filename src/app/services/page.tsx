81c114cb32a5c17641047814392e89b3b4e4267d
Kaveen Thilakarathna <kaviyasliit@gmail.com>
2025-08-16 17:54:29 +0000

    in this services page  bottom add our cta section

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,13 +1,17 @@
 
 import { MotionWrapper } from "@/components/motion-wrapper";
+import { HomepageCta } from "@/components/sections/homepage-cta";
 import { ServicesHero } from "@/components/sections/services-hero";
 import { ServicesList } from "@/components/sections/services-list";
+import { getHeroContent } from "@/services/firestore";
 
-export default function ServicesPage() {
+export default async function ServicesPage() {
+    const heroContent = await getHeroContent();
     return (
         <MotionWrapper>
             <ServicesHero />
             <ServicesList />
+            {heroContent && <HomepageCta ctaSection={heroContent.ctaSection} />}
         </MotionWrapper>
     );
 }
