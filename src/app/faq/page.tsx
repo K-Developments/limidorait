18e612d9abc9c2fb79bb7baa56ea583632736218
Kaveen Thilakarathna <kaviyasliit@gmail.com>
2025-08-18 04:55:00 +0000

    in this faq page bottom also add our cta section

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -3,17 +3,21 @@ import { Faq } from "@/components/sections/faq";
 import { MotionWrapper } from "@/components/motion-wrapper";
 import { FaqHero } from "@/components/sections/faq-hero";
 import type { Metadata } from 'next';
+import { HomepageCta } from "@/components/sections/homepage-cta";
+import { getHeroContent } from "@/services/firestore";
 
 export const metadata: Metadata = {
   title: 'FAQs | Limidora Digital',
   description: 'Find answers to frequently asked questions about our services, development process, project costs, and more. Get the information you need from Limidora.',
 };
 
-export default function FaqPage() {
+export default async function FaqPage() {
+    const heroContent = await getHeroContent();
     return (
         <MotionWrapper>
             <FaqHero />
             <Faq />
+            {heroContent && <HomepageCta ctaSection={heroContent.ctaSection} />}
         </MotionWrapper>
     );
 }
