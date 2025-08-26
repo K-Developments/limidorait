f20ba1487b195739d4ccffe79dfb6d6c1a3d191d
Kaveen Thilakarathna <kaviyasliit@gmail.com>
2025-08-26 11:35:45 +0000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next';
 import HomePageClient from "./home-page-client";
 import { PublicLayout } from "./public-layout";
 
-export const dynamic = 'force-static';
+export const revalidate = false;
 
 export const metadata: Metadata = {
   title: {
