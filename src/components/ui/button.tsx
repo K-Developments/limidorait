c52f25fe1fb98fa98383df2b4d50d9d4c3d83d93
Kaveen Thilakarathna <kaviyasliit@gmail.com>
2025-08-26 13:57:39 +0000

    this button decoration border makes a 1px solid border

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "relative btn-corners bg-primary text-primary-foreground hover:bg-primary/90",
+          "relative btn-corners border-transparent bg-primary text-primary-foreground hover:bg-primary/90",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
