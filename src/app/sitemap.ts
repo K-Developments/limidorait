7b1b0b9df74bb273ebb224378fa35855f79082f0
Kaveen Thilakarathna <kaviyasliit@gmail.com>
2025-08-29 04:58:55 +0000

    now we have to make this site top level seo friendly . top achive this m

---
+++ src/app/sitemap.ts
@@ -0,0 +1,42 @@
+
+import type { MetadataRoute } from 'next';
+import { getProjects, getServices } from '@/services/firestore';
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://limidora.com';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const lastModified = new Date();
+
+  // Fetch dynamic routes
+  const projects = await getProjects();
+  const services = await getServices();
+
+  const projectUrls = projects.map((project) => ({
+    url: `${BASE_URL}${project.link}`,
+    lastModified,
+  }));
+
+  const serviceUrls = services.map((service) => ({
+    url: `${BASE_URL}${service.link}`,
+    lastModified,
+  }));
+
+  // Define static routes
+  const staticRoutes = [
+    '/',
+    '/about',
+    '/portfolio',
+    '/services',
+    '/faq',
+    '/contact',
+    '/privacy-policy',
+    '/terms-of-service',
+  ];
+
+  const staticUrls = staticRoutes.map((route) => ({
+    url: `${BASE_URL}${route}`,
+    lastModified,
+  }));
+
+  return [...staticUrls, ...projectUrls, ...serviceUrls];
+}
